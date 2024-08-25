d0c7b9048e5b16d77e2cee31de13f23b4e766ed1
Akhil <akhil.donthula8977@gmail.com>
2024-08-25 13:51:32 +0530

    url changes

--- index.mjs
+++ index.mjs
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
     res.status(200).json({"operation_code":1});
 });
 app.post('/data',postController);
+app.post('/bfhl',postController);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
