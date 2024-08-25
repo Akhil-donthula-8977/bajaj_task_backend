9aa162b9cb105d30f71f8e6044ab15cd34eb2eac
Akhil <akhil.donthula8977@gmail.com>
2024-08-25 12:36:40 +0530

    cors addded

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
-app.use(cors())
+app.use(cors());
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
