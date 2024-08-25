6f06aaeccf4cdcc3a890837f16f7d204280f4b99
Akhil <akhil.donthula8977@gmail.com>
2024-08-25 14:08:08 +0530

    url changes

--- index.mjs
+++ index.mjs
@@ -11,8 +11,16 @@ import { postController } from './controllers/testControllers.mjs';
 app.get('/', (req, res) => {
     res.status(200).json({"operation_code":1});
 });
+app.get('/bfhl',(req, res) => {
+    res.status(200).json({"operation_code":1});
+});
+app.get('/data/bfhl',(req, res) => {
+    res.status(200).json({"operation_code":1});
+});
 app.post('/data',postController);
+app.post('/data/bfhl',postController);
 app.post('/bfhl',postController);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
